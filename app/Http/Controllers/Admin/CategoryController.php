6ffae3fc354e6abea4805ac33424e1ee243344f2
Patrick Joel <joekenpat@gmail.com>
2020-12-18 15:01:53 +0100

    fixed route param name

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -66,14 +66,14 @@ class CategoryController extends Controller
    * @param Str $subcat_slug
    * @return \Illuminate\Http\Response
    */
-  public function update(Request $request, $cat_slug)
+  public function update(Request $request, $category_slug)
   {
     $this->validate($request, [
       'name' => 'sometimes|nullable|string|min:3|max:245',
       'icon' => 'sometimes|nullable|image|mimes:png,jpg,svg,jpeg,gif',
     ]);
 
-    $category = Category::whereSlug($cat_slug)->firstOrFail();
+    $category = Category::whereSlug($category_slug)->firstOrFail();
     if (isset($request->name)) {
       $category->name = $request->name;
     }
