e852457223210d58ac05ef8ebc29a0f9643f17e7
Patrick Joel <joekenpat@gmail.com>
2020-12-23 20:28:13 +0100

    mail fixes

--- app/Mail/OrderConfirmed.php
+++ app/Mail/OrderConfirmed.php
@@ -2,6 +2,8 @@
 
 namespace App\Mail;
 
+use App\Models\Order;
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -9,25 +11,30 @@ use Illuminate\Queue\SerializesModels;
 
 class OrderConfirmed extends Mailable
 {
-    use Queueable, SerializesModels;
+  use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+  protected $user, $order;
+  /**
+   * Create a new message instance.
+   *
+   * @return void
+   */
+  public function __construct(User $user, Order $order)
+  {
+    $this->user = $user;
+    $this->order = $order;
+  }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
-    {
-        return $this->markdown('emails.orders.confirmed');
-    }
+  /**
+   * Build the message.
+   *
+   * @return $this
+   */
+  public function build()
+  {
+    return $this->markdown('emails.orders.confirmed', [
+      'user' => $this->user,
+      'order' => $this->order,
+    ]);
+  }
 }
